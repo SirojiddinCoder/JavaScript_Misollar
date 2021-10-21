ba468306a5dc2d9954c5c2e8ee084f0a5c37a774
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-10-21 11:19:32 -0700

    js misollar yakunlandi

--- javaScript.js
+++ javaScript.js
@@ -94,3 +94,145 @@
 // var K=((a ** 2 )+ (b ** 2)) ** 0.5;
 
 // console.log("O'rta Geometrigi="+ K);
+
+
+
+// Begin - 10
+
+// var a=parseFloat(prompt(" 1-sonni kiriting "));
+
+// var b=parseFloat(prompt(" 2-sonni kiriting "));
+
+// var S= a + b;
+// var L= a * b;
+// var A= a * a;
+// var B= b * b;
+
+// alert("Yigindisi="+S+","+"Kopaytmasi="+L+","+"A ni kvadrati="+A+","+"B ni kvadrati="+B);
+
+
+// Begin - 11
+
+// var a=parseFloat(prompt(" 1-sonni kiriting "));
+
+// var b=parseFloat(prompt(" 2-sonni kiriting "));
+
+// var S= Math.abs(a + b);
+// var L= a * b;
+// var A= a * a;
+// var B= b * b;
+
+// alert("Yigindisi="+S+","+"Kopaytmasi="+L+","+"A ni kvadrati="+A+","+"B ni kvadrati="+B);
+
+
+// Begin - 12
+
+// var a=parseFloat(prompt(" 1-tomonni  kiriting "));
+
+// var b=parseFloat(prompt(" 2-tomonni  kiriting "));
+
+// c=((a ** 2 )+ (b ** 2)) ** 0.5;
+// P = a + b + c;
+
+// alert("gipotenuzasi="+c+","+"Peremetri="+P);
+
+
+// Begin - 13
+
+// var R1=parseFloat(prompt(" 1-Radiusni  kiriting "));
+
+// var R2=parseFloat(prompt(" 2-Radiusni  kiriting "));
+// pi = 3.14;
+
+// S1 =pi * R1;
+// S2 =pi * R2;
+// S3 =pi * (R1 - R2);
+
+
+// alert("S1 yuza="+S1+",         "+"S2 yuza="+S2+",       "+"S3 yuza="+S3);
+
+// Begin - 14
+
+// var L=parseFloat(prompt("Doiraning Uzunligini kiriting "));
+// var pi = 3.14;
+
+// var R = L /(2 * pi);
+// var S = pi * R * R;
+// alert("Radiusi="+R+"  ,  "+"Tola Yuzasi="+S);
+
+
+// Begin - 16
+
+// var X1=parseFloat(prompt("Birinchi nuqtani  kiriting "));
+// var X2=parseFloat(prompt("Ikkinchi nuqtani  kiriting "));
+
+//  S = Math.abs(X2 - X1);
+// alert("Ikki Nuqta orasidagi Masofa="+S);
+
+//Begin - 17
+
+// var x=parseFloat(prompt("Birinchi nuqtani  kiriting "));
+// var y=parseFloat(prompt("Ikkinchi nuqtani  kiriting "));
+
+
+// var AC = x;
+// var CB = y;
+// var AB = z;
+// k = x + y;
+// T = x + y + z;
+// alert("AC ="+x+"  ,  "+"CB="+S);
+
+// Begin - 18
+
+// var x=parseFloat(prompt("Birinchi nuqtani  kiriting "));
+// var y=parseFloat(prompt("Ikkinchi nuqtani  kiriting "));
+
+
+// var AC = x;
+// var CB = y;
+// var AB = z;
+// k = x * y;
+
+// alert("kopaytmasi="+k);
+
+
+// Begin 20
+
+// var x1=parseInt(prompt("x1="));
+// var y1=parseInt(prompt("y1="));
+
+// var x2=parseInt(prompt("x2="));
+// var y2=parseInt(prompt("y2="));
+
+// S = (((x2 - x1)*(x2 - x1) +(y2 - y1)*(y2 - y1) )**0.5);
+
+// console.log("Ikki nuqta orasidagi masofa=" +S);
+
+
+
+// Begin 21
+
+// var a=parseInt(prompt("a="));
+// var b=parseInt(prompt("b="));
+
+// var c=parseInt(prompt("c="));
+
+
+// p = a + b + c;
+// s = ((p * (p - a)*(p - b)-(p - c))**0.5)
+
+// alert("Peremetri ="+p+"  ,  "+"yuzasi="+s);
+
+
+//Begin 25
+
+// var x=parseInt(prompt("x="));
+// y = 3 * x *x *x *x *x *x - 6 * x * x - 7;
+// alert("Y qiymati="+y);
+
+
+//Begin 26
+
+// var x=parseInt(prompt("x="));
+// y =( 4 * (x - 3)*(x - 3)*(x - 3)(x - 3)*(x - 3)*(x - 3) - (7*(x - 3)*(x - 3)*(x - 3)) + 2);
+// alert("Y qiymati="+y);
