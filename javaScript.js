4b324c7728af550a27e7a9669c3d04585a6ed8bb
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-10-21 07:30:29 -0700

    9 ta misol yechildi

---
+++ javaScript.js
@@ -0,0 +1,132 @@
+// Begin-1
+
+// var a=parseFloat(prompt("Kvadrat tomonini kiriting "));
+// var p=4*a;
+// console.log("Perimetr="+p);
+
+
+// Begin - 2
+
+// var a=parseFloat(prompt("Kvadrat tomonini kiriting "));
+// var s=a * a;
+// console.log("Yuza="+s);
+
+
+
+// Begin - 3
+
+// var a=parseFloat(prompt("tortburchakni 1-tomonini kiriting "));
+
+// var b=parseFloat(prompt("tortburchakni 2-tomonini kiriting "));
+
+// var s=a * b;
+// var P=2*(a + b);
+// console.log("Yuza="+s+","+"Peremetri="+P);
+
+
+
+// Begin - 4
+
+// var d=parseFloat(prompt("Aylananing Diametrini kiriting "));
+// var pi = 3.14;
+// var L= pi * d;
+// console.log("Uzunligi="+L);
+
+
+// Begin - 5
+
+// var a=parseFloat(prompt("Kubning tomonini kiriting "));
+
+// var V=a*a*a;
+// var S=6*(a * a);
+// console.log("Hajmi="+V+","+"Tola Sirti="+S);
+
+
+
+
+
+// Begin - 6
+
+// var a=parseFloat(prompt("Paralepipedni 1-tomonini kiriting "));
+
+// var b=parseFloat(prompt("Paralepipedni 2-tomonini kiriting "));
+
+// var c=parseFloat(prompt("Paralepipedni 3-tomonini kiriting  "));
+
+// var V=a*b*c;
+// var S=2*(a * b + b * c + a * c);
+// console.log("Hajmi="+V+","+"Tola Sirti="+S);
+
+
+
+
+
+// Begin - 7
+
+// var R=parseFloat(prompt("Doiraning Radiusini kiriting "));
+// var pi = 3.14;
+
+// var L= 2 * pi * R;
+// var S= pi * R * R;
+// console.log("Uzunligi="+L+","+"Tola Yuzasi="+S);
+
+
+
+
+// Begin - 8
+
+// var a=parseFloat(prompt(" 1-sonni kiriting "));
+
+// var b=parseFloat(prompt(" 2-sonni kiriting "));
+
+// var K=(a + b)/2;
+
+// alert("O'rta Arifmetigi="+ K);
+
+
+
+// Begin - 9
+
+// var a=parseFloat(prompt(" 1-sonni kiriting "));
+
+// var b=parseFloat(prompt(" 2-sonni kiriting "));
+
+// var K=((a ** 2 )+ (b ** 2)) ** 0.5;
+
+// console.log("O'rta Geometrigi="+ K);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var a=parseInt(prompt("Sonni kiritng"));
+// var birliklar=parseInt(a/1)%10;
+// var unliklar=parseInt(a/10)%10;
+// var yuzliklar=parseInt(a/100)%10;
+// var res=birliklar+unliklar+yuzliklar;
+// alert("Raqamlari yig'gindis "+res+" ga teng");
